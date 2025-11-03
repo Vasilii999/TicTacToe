4cce6b73751d50e478f137217613e059feff047e
Vasilii999 <kuksinvasilii@gmail.com>
2025-11-03 17:08:31 +0600

    test

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'page/application/application.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 
-
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await MobileAds.instance.initialize();
